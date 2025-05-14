f690c51211f50c9e05384a5e20fc0d701d638982
廉志远 <lzyoo666@outlook.com>
2025-05-14 10:43:04 +0800

    嘻嘻

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,38 @@
 import { defineConfig } from 'cz-git'
 
+
 export default defineConfig({
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+    // @see: https://commitlint.js.org/#/reference-rules
+    "body-leading-blank": [2, "always"],
+    "footer-leading-blank": [1, "always"],
+    "header-max-length": [2, "always", 108],
+    "subject-empty": [2, "never"],
+    "type-empty": [2, "never"],
+    "subject-case": [0],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+        "wip",
+        "workflow",
+        "types",
+        "release"
+      ]
+    ]
+  },
     prompt:{
         messages: {
             type: '选择你要提交的类型 :',
