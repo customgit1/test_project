03e47a5448b45d59125f97c90c0f8a1440873d41
廉志远 <lzyoo666@outlook.com>
2025-05-16 09:25:27 +0800

    ci: 添加代码提交规范和 lint 配置
    - 新增 commit-msg 和 pre-commit 钩子
    - 添加 commitlint 配置文件
    - 新增 ESLint 配置文件
    - 更新 package.json 中的脚本和依赖

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import eslintConfig from "@antfu/eslint-config"
+
+export default eslintConfig({
+  stylistic: {
+    indent: 2,
+    quotes: "double",
+    semi: false,
+  },
+  vue: {
+    overrides: {
+      // 强制 <script setup> 中的宏排序
+      "vue/define-macros-order": ["error", {
+        order: [
+          "defineOptions",
+          "defineModel",
+          "defineProps",
+          "defineEmits",
+          "defineSlots",
+        ],
+        defineExposeLast: true, // defineExpose 必须在最后
+      }],
+      // 强制 Vue 文件中的块顺序
+      "vue/block-order": ["error", {
+        order: [
+          "template",
+          "script[setup]",
+          "style",
+        ],
+      }],
+    },
+  },
+  ignores: [
+    "dist/**",
+    "node_modules/**",
+  ],
+})
